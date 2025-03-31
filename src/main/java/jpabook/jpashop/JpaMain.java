abf88d8c507125b61e7e63922852bd722044b3dc
JJONG\ljm17 <ljm920604@gmail.com>
2025-03-31 22:54:07 +0900

    Feat. 초기 세팅 및 준비

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Order order = em.find(Order.class, 1L);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            // entityManager가 connection을 갖고 있어서 꼭 닫아줘야ㅕ함.
+            em.close();
+        }
+
+        emf.close();
+    }
+}
