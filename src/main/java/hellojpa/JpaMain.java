226ca9a920feb1b824c6c92d24eb40f5da7429b1
JJONG\ljm17 <ljm920604@gmail.com>
2025-04-05 16:57:25 +0900

    Feat. 양방향 관계 실수 많이 하는 부분 - 연관관계의 주인 아닌 곳에 값을 넣음

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,32 +19,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-//            이 시점에서는 단방향 관계만 설정됩니다: Member → Team
-            member.setTeam(team);
-
-//            그러나 반대 방향의 관계(Team → Members)는 메모리에서 자동으로 업데이트되지 않습니다.
             em.persist(member);
+
+
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            team.getMembers().add(member);
+            em.persist(team);
+
 
             em.flush(); // 영속성 컨텍스트를 데이터베이스와 동기화
             em.clear(); // 영속성 컨텍스트를 비워서 DB에서 새로 로드하도록 함
-
-            Member findMember = em.find(Member.class, member.getId());
-            System.out.println("findMember = " + findMember);
-            List<Member> members = findMember.getTeam().getMembers();
-            System.out.println("members = " + members);
-            for (Member m : members) {
-                System.out.println("m = " + m.getUsername());
-            }
-
-
-            
 
             tx.commit();
         } catch (Exception e) {
